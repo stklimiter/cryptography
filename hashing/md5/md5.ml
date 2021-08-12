abd095cd50ed88e18c5ce24299d736fc205d0296
ristim <32619521+ristim@users.noreply.github.com>
2021-08-12 22:17:55 +0200

    MD-5 | ML | renamed because dune can't handle dashes

--- hashing/md5/md5.ml
+++ hashing/md5/md5.ml
@@ -5,7 +5,11 @@ let append (value: string): string = value ^ "do stuff"
 
 let bytes_toString (value: bytes): string = Bytes.to_string value
 
+
 let append_bytes (value: string ): bytes = Bytes.of_string value
+
+
+
 
 let () = print_endline (bytes_toString (append_bytes "Hello, Tim2!"))
 
