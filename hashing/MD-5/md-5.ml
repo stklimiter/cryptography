59cca64d7a64cd9b1921b66afda1576c91bdfcb4
ristim <32619521+ristim@users.noreply.github.com>
2021-08-12 22:06:03 +0200

    MD-5 | ML | first commit

---
+++ hashing/MD-5/md-5.ml
@@ -0,0 +1,11 @@
+let remainingturns = input_line stdin
+
+
+let append (value: string): string = value ^ "do stuff"
+
+let bytes_toString (value: bytes): string = Bytes.to_string value
+
+let append_bytes (value: string ): bytes = Bytes.of_string value
+
+let () = print_endline (bytes_toString (append_bytes "Hello, Tim2!"))
+
