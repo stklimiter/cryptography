c8c0ce034d9beb496b2262b268ef44428be09b8a
ristim <32619521+ristim@users.noreply.github.com>
2021-08-17 00:47:49 +0200

    MD-5 | ML | pipeline is the idiomatic way to combine functions

--- hashing/md5/md5.ml
+++ hashing/md5/md5.ml
@@ -1,4 +1,4 @@
 
-let () =  let  bitList = Bit.string_to_bit_list (input_line stdin)  in 
-            let appendingBits  = bitList @ Bit.need_append (Bit.count_bits (bitList)) in
-             print_endline (Bit.bit_list_to_string appendingBits);
+let _ = let bitList = Bit.string_to_bit_list (input_line stdin)  in
+            let appendingBits  =  bitList |> Bit.count_bits |> (bitList @ Bit.need_append) in
+                appendingBits |> Bit.bit_list_to_string |> print_endline;
