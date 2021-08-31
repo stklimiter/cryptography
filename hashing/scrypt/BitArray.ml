e8241cca33dd882b7d70f72f0f5eedc68fe38963
ristim <32619521+ristim@users.noreply.github.com>
2021-08-31 23:12:27 +0200

    scrypt | ML | added a size to bitArray

--- hashing/scrypt/BitArray.ml
+++ hashing/scrypt/BitArray.ml
@@ -29,8 +29,9 @@ let string_to_byte value = Bytes.of_string value *)
 
 type bit = Zero | One
 
-type bitList = bit list 
+type bitList = (bit list * int)
 
+(* print functions *)
 let to_int = function 
                      | One -> 1
                      | Zero -> 0
@@ -41,17 +42,28 @@ let to_string = function
 
 let print_bit bit =  bit |> to_int  |> print_int 
 
-let rec print_byte_array = function
+let rec flatten' list (totalList , size) = match list with 
+                        | [] -> (totalList, size)
+                        | (head , sizehead) :: tail -> flatten' tail (head @ totalList , size + sizehead)
+
+let flatten list = flatten' list ([],  0)
+
+let rec print_byte_array'  = function
                                     | [] -> ()
-                                    | head :: tail -> print_bit head; print_byte_array tail
+                                    | head :: tail -> print_bit head; print_byte_array' tail
+
+                                    
+let print_byte_array (list, size) = print_endline ("size: " ^ string_of_int  size);
+                                            print_byte_array' list
 
 
+(* print functions *)
 let rec int_to_bitList' value bitList bitsLeft =     if (bitsLeft = 0) 
                                                             then bitList
                                                             else int_to_bitList' (value  / 2 ) ([if ((value mod 2) = 1) then One else Zero] @ bitList) (bitsLeft - 1)
-let int_to_bitList int = int_to_bitList' int [] 8
+let int_to_bitList int = ((int_to_bitList' int []  8), 8)
 
-let int_list_to_bitList int_list = List.map int_to_bitList int_list |> List.flatten 
+let int_list_to_bitList int_list = List.map int_to_bitList int_list |> flatten 
 
 let split_on_char (value: string): char list = List.init (String.length value) (String.get value) 
 
